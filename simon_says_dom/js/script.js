c4ab72f7e74f5806c65bee57745c261c54438e49
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-20 17:25:33 +0100

    add human code

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,10 @@
+// gestione generazione numeri, tenerli a schermo per 30 secondi e salvataggio in un array
+    // recupero elementi da html
+        // lista per generare items
+        const 
+
+
+// gestine inserimento utente e salvtaggio in array
+
+
+// confronto tra array e stampa risultato
