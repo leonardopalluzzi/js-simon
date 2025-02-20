8da0a6ffab97526a8a62c9f597b4d8f215840e5d
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-20 17:33:21 +0100

    add random number generator

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -1,7 +1,23 @@
 // gestione generazione numeri, tenerli a schermo per 30 secondi e salvataggio in un array
-    // recupero elementi da html
-        // lista per generare items
-        const 
+// recupero elementi da html
+// lista per generare items
+const numbersListEl = document.getElementById('numbers-list');
+
+// generare numeri
+numbersListEl.innerHTML = 'ciao;'
+
+function numbersGenerator() {
+    const numbersList = [];
+    for(let i = 0; i < 5; i++){
+        let thisNumber = Math.floor(Math.random() * 50);
+        numbersList[i] = thisNumber;
+    }
+    return numbersList;
+}
+
+numbersListEl.innerHTML = numbersGenerator();
+        
+        
 
 
 // gestine inserimento utente e salvtaggio in array
