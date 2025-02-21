7b3d191c254c3234a6f8ee6332964efe5e30b810
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-21 02:34:18 +0100

    add start buttoin and time selector

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -6,6 +6,19 @@ const userInput = document.querySelectorAll('.form-control');
 const buttonEl = document.querySelector('.btn');
 const mainText = document.getElementById('instructions');
 const inputBox = document.getElementById('input-group');
+const startBtn = document.querySelector('.start_game');
+const startForm = document.querySelector('.start_form');
+const timeValue = document.querySelector('.form-select');
+const timeSelector = document.querySelector('.select');
+
+
+// funzione di assegnazione tempo
+function time(timeChoice) {
+    const currentTime = Number(timeChoice.value * 1000);
+    console.log(currentTime);
+    
+    return currentTime;
+}
 
 // funzione per generare numeri
 function numbersGenerator() {
@@ -47,23 +60,23 @@ function getUserNumbers(arr) {
     return userNumberList;
 }
 
-function inputCheck (userNumbers, userArray){
+function inputCheck(userNumbers, userArray) {
     let message = '';
     let errorFlag = 0;
-    for (let i = 0; i < userNumbers.length; i++){
+    for (let i = 0; i < userNumbers.length; i++) {
         let inputElement = userArray[i];
         let currentNumber = userNumbers[i];
         // controlla che non ci siano due valori uguali
-        for(let j = i + 1; j < userNumbers.length; j++){
-            if(currentNumber == userNumbers[j]){
+        for (let j = i + 1; j < userNumbers.length; j++) {
+            if (currentNumber == userNumbers[j]) {
                 errorFlag = 3;
             }
         }
         // controlla che venga inserito un numero e non altro input
-        if(isNaN(currentNumber)){
+        if (isNaN(currentNumber)) {
             errorFlag = 1;
-        // controlla che il form non venga hackerato per il required
-        } else if (!inputElement.hasAttribute('required')){
+            // controlla che il form non venga hackerato per il required
+        } else if (!inputElement.hasAttribute('required')) {
             errorFlag = 2;
         }
     }
@@ -85,35 +98,47 @@ function getResults(userArr, initialArr) {
     return rightCounter;
 }
 
-// salvare i numeri generati in un array fisso e inserirli in pagina
-const generatedNumbers = numbersGenerator();
-numbersListEl.innerHTML = generatedNumbers;
-console.log(generatedNumbers);
 
-// tenere i numeri a schermo per 30 secondi
-setTimeout(() => {
-    numbersListEl.classList.add('d-none');
-    answerFormEl.classList.remove('d-none');
-    mainText.innerHTML = 'inserici i numeri che hai visto poco fa';
+mainText.classList.add('d-none')
 
-}, 3000);
+// start button
+startForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    startBtn.classList.add('d-none');
+    timeSelector.classList.add('d-none');
+    mainText.classList.remove('d-none');
+
+    // salvare i numeri generati in un array fisso e inserirli in pagina
+    const generatedNumbers = numbersGenerator();
+    numbersListEl.innerHTML = generatedNumbers;
+    console.log(generatedNumbers);
+
+    // tenere i numeri a schermo per 30 secondi
+    setTimeout(() => {
+        numbersListEl.classList.add('d-none');
+        answerFormEl.classList.remove('d-none');
+        mainText.innerHTML = 'inserici i numeri che hai visto poco fa';
+
+    }, time(timeValue));
+})
+
 
 // trigger creazione array utente su submit
-formEl.addEventListener('submit', function handler (e) {
+formEl.addEventListener('submit', function handler(e) {
     e.preventDefault();
     console.log(getUserNumbers(userInput));
 
     // confronto tra array e stampa risultato
     console.log(getResults(getUserNumbers(userInput), generatedNumbers));
-    if(inputCheck(getUserNumbers(userInput), userInput) == 1){
+    if (inputCheck(getUserNumbers(userInput), userInput) == 1) {
         alert('per favore, non mi hackerare il form ed inserisci solo numeri');
         location.reload();
         location.reload();
-    } else if(inputCheck(getUserNumbers(userInput), userInput) == 2){
+    } else if (inputCheck(getUserNumbers(userInput), userInput) == 2) {
         alert('per favore non mi hackerare il form');
         location.reload();
         location.reload();
-    } else if(inputCheck(getUserNumbers(userInput), userInput) == 3){
+    } else if (inputCheck(getUserNumbers(userInput), userInput) == 3) {
         alert('non inserire due valori uguali');
         location.reload();
         location.reload();
